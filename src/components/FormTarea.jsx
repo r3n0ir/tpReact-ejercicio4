dafa9b54c558e1a26605a13f418bb091d7cd7c9b
Elian Miranda <elianmir@outlook.com>
2024-09-22 19:24:29 -0300

    lista de tareas agregada

--- src/components/FormTarea.jsx
+++ src/components/FormTarea.jsx
@@ -1,6 +1,7 @@
 //import Button from 'react-bootstrap/Button';
 //import Form from 'react-bootstrap/Form';
 import { Form, Button } from "react-bootstrap";
+import ListaTareas from "./ListaTareas";
 
 const FormTarea = () => {
   return (
@@ -13,6 +14,7 @@ const FormTarea = () => {
           </Button>
         </Form.Group>
       </Form>
+      <ListaTareas></ListaTareas>
     </section>
   );
 };
